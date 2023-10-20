36d37122b25ef434a2ec762a4050ec54b20a6141
unknown <louieconcepcion18@gmail.com.com>
2023-10-20 16:04:15 +0800

    new

--- server/server.js
+++ server/server.js
@@ -11,12 +11,19 @@ import nodemailer from "nodemailer"
 
 dotenv.config();
 const app = express();
+const allowedOrigins = process.env.ALLOWED_ORIGINS || 'https://bul-vroom-1.vercel.app';
+
 app.use(cors({
-  origin: 'https://bul-vroom-1.vercel.app/',
-  credentials: true, 
-}
-    
-));
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static('public'));
