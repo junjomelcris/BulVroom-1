13b330b73eff588f80003403a2508c93018a59f4
unknown <louieconcepcion18@gmail.com.com>
2023-10-23 23:22:06 +0800

    up

--- server/Routes/Admin/Admin.js
+++ server/Routes/Admin/Admin.js
@@ -11,6 +11,18 @@ import nodemailer from "nodemailer"
 const router = express.Router();
 import con from '../database.js';
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
 const verifyUser = (req, res, next) => {
     const token = req.cookies.token;
     if(!token) {
