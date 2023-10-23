05381d765a5f6ce49ca9cf4c3b3f07749cedb3f4
unknown <louieconcepcion18@gmail.com.com>
2023-10-23 23:09:28 +0800

    gagsti

--- server/Routes/Admin/Admin.js
+++ server/Routes/Admin/Admin.js
@@ -9,7 +9,7 @@ import path from 'path'
 import dotenv from 'dotenv'
 import nodemailer from "nodemailer"
 const router = express.Router();
-import con from '../database';
+import con from '../database.js';
 
 const verifyUser = (req, res, next) => {
     const token = req.cookies.token;
