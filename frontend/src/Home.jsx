f0b93e9d13c462993fcd02c89b5d3e1034d690f6
junjomelcris <junjomelcris>
2023-11-16 23:29:56 +0800

    push

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,70 +1,58 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import { Bar, Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 
 function Home() {
-  const [adminCount, setAdminCount] = useState(0);
-  const [employeeCount, setEmployeeCount] = useState(0);
-  const [vCount, setvCount] = useState(0);
   const [approvedUserCount, setApprovedUserCount] = useState(0);
   const [pendingUserCount, setPendingUserCount] = useState(0);
   const [approvedVehicleCount, setApprovedVehicleCount] = useState(0);
   const [pendingVehicleCount, setPendingVehicleCount] = useState(0);
   const [transactionData, setTransactionData] = useState([]);
 
-  const chartRefUsers = useRef(null);
-  const chartRefVehicles = useRef(null);
-  const chartRefTransactions = useRef(null);
+  const userChartRef = useRef(null);
+  const vehicleChartRef = useRef(null);
+  const transactionChartRef = useRef(null);
+  const userChartInstance = useRef(null);
+  const vehicleChartInstance = useRef(null);
+  const transactionChartInstance = useRef(null);
 
   useEffect(() => {
     // Fetch data and set state
-    axios.get('https://bulvroom.onrender.com/adminCount')
+    axios
+      .get('https://bulvroom.onrender.com/approvedPendingUserCount')
       .then((res) => {
-        setAdminCount(res.data[0].admin);
+        setApprovedUserCount(res.data.approved);
+        setPendingUserCount(res.data.pending);
       })
       .catch((err) => console.log(err));
 
-    axios.get('https://bulvroom.onrender.com/userCount')
+    axios
+      .get('https://bulvroom.onrender.com/approvedPendingVehicleCount')
       .then((res) => {
-        setEmployeeCount(res.data[0].users);
+        setApprovedVehicleCount(res.data.approved);
+        setPendingVehicleCount(res.data.pending);
       })
       .catch((err) => console.log(err));
 
-    axios.get('https://bulvroom.onrender.com/vCount')
+    axios
+      .get('https://bulvroom.onrender.com/transactions')
       .then((res) => {
-        setvCount(res.data[0].vehicles);
-      })
-      .catch((err) => console.log(err));
-
-    // Fetch data for approved/pending users
-    axios.get('https://bulvroom.onrender.com/approvedPendingUserCount')
-      .then((res) => {
-        setApprovedUserCount(res.data[0].approvedUsers);
-        setPendingUserCount(res.data[0].pendingUsers);
-      })
-      .catch((err) => console.log(err));
-
-    // Fetch data for approved/pending vehicles
-    axios.get('https://bulvroom.onrender.com/approvedPendingVehicleCount')
-      .then((res) => {
-        setApprovedVehicleCount(res.data[0].approvedVehicles);
-        setPendingVehicleCount(res.data[0].pendingVehicles);
-      })
-      .catch((err) => console.log(err));
-
-    // Fetch data for transactions
-    axios.get('https://bulvroom.onrender.com/transactions')
-      .then((res) => {
+        // Assuming transaction data is an array of numbers
         setTransactionData(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
-    // Create a new chart instance for approved/pending users
-    if (chartRefUsers.current) {
-      const ctxUsers = chartRefUsers.current.getContext('2d');
-      new Chart(ctxUsers, {
+    // Create or update user count chart
+    if (userChartRef.current) {
+      if (userChartInstance.current) {
+        userChartInstance.current.destroy();
+      }
+
+      const ctx = userChartRef.current.getContext('2d');
+      userChartInstance.current = new Chart(ctx, {
         type: 'bar',
         data: {
           labels: ['Approved Users', 'Pending Users'],
@@ -72,10 +60,10 @@ function Home() {
             {
               label: 'Count',
               backgroundColor: ['#28A745', '#FFC107'],
-              borderColor: 'rgba(40,167,69,0.8)',
+              borderColor: 'rgba(0,123,255,0.8)',
               borderWidth: 1,
               hoverBackgroundColor: ['#218838', '#FFA000'],
-              hoverBorderColor: 'rgba(40,167,69,1)',
+              hoverBorderColor: 'rgba(0,123,255,1)',
               data: [approvedUserCount, pendingUserCount],
             },
           ],
@@ -89,11 +77,17 @@ function Home() {
         },
       });
     }
+  }, [approvedUserCount, pendingUserCount]);
 
-    // Create a new chart instance for approved/pending vehicles
-    if (chartRefVehicles.current) {
-      const ctxVehicles = chartRefVehicles.current.getContext('2d');
-      new Chart(ctxVehicles, {
+  useEffect(() => {
+    // Create or update vehicle count chart
+    if (vehicleChartRef.current) {
+      if (vehicleChartInstance.current) {
+        vehicleChartInstance.current.destroy();
+      }
+
+      const ctx = vehicleChartRef.current.getContext('2d');
+      vehicleChartInstance.current = new Chart(ctx, {
         type: 'bar',
         data: {
           labels: ['Approved Vehicles', 'Pending Vehicles'],
@@ -101,10 +95,10 @@ function Home() {
             {
               label: 'Count',
               backgroundColor: ['#28A745', '#FFC107'],
-              borderColor: 'rgba(40,167,69,0.8)',
+              borderColor: 'rgba(0,123,255,0.8)',
               borderWidth: 1,
               hoverBackgroundColor: ['#218838', '#FFA000'],
-              hoverBorderColor: 'rgba(40,167,69,1)',
+              hoverBorderColor: 'rgba(0,123,255,1)',
               data: [approvedVehicleCount, pendingVehicleCount],
             },
           ],
@@ -118,58 +112,77 @@ function Home() {
         },
       });
     }
+  }, [approvedVehicleCount, pendingVehicleCount]);
 
-    // Create a new chart instance for transactions
-    if (chartRefTransactions.current) {
-      const ctxTransactions = chartRefTransactions.current.getContext('2d');
-      new Chart(ctxTransactions, {
+  useEffect(() => {
+    // Create or update transaction line chart
+    if (transactionChartRef.current) {
+      if (transactionChartInstance.current) {
+        transactionChartInstance.current.destroy();
+      }
+
+      const ctx = transactionChartRef.current.getContext('2d');
+      transactionChartInstance.current = new Chart(ctx, {
         type: 'line',
         data: {
-          labels: transactionData.map((transaction) => transaction.date),
+          labels: Array.from({ length: transactionData.length }, (_, i) => i + 1),
           datasets: [
             {
               label: 'Transactions',
               borderColor: '#007BFF',
               borderWidth: 1,
+              data: transactionData,
               fill: false,
-              data: transactionData.map((transaction) => transaction.amount),
             },
           ],
         },
         options: {
           scales: {
             x: {
-              type: 'category',
+              type: 'linear',
+              position: 'bottom',
             },
           },
         },
       });
     }
-  }, [approvedUserCount, pendingUserCount, approvedVehicleCount, pendingVehicleCount, transactionData]);
+  }, [transactionData]);
 
   return (
     <div>
-      {/* Existing code for Admin, Verified Users, and Verified Vehicles */}
-
-      {/* Bar Chart for Approved/Pending Users */}
       <div className='mt-4 px-5 pt-3'>
-        <h3>Users Statistics</h3>
-        <canvas ref={chartRefUsers} width='7' height='1'></canvas>
+        <h3>User Count Statistics</h3>
+        <canvas ref={userChartRef} width='7' height='1'></canvas>
       </div>
 
-      {/* Bar Chart for Approved/Pending Vehicles */}
       <div className='mt-4 px-5 pt-3'>
-        <h3>Vehicles Statistics</h3>
-        <canvas ref={chartRefVehicles} width='7' height='1'></canvas>
+        <h3>Vehicle Count Statistics</h3>
+        <canvas ref={vehicleChartRef} width='7' height='1'></canvas>
       </div>
 
-      {/* Line Chart for Transactions */}
       <div className='mt-4 px-5 pt-3'>
-        <h3>Transactions Statistics</h3>
-        <canvas ref={chartRefTransactions} width='7' height='1'></canvas>
+        <h3>Transaction Statistics</h3>
+        <canvas ref={transactionChartRef} width='7' height='1'></canvas>
       </div>
 
-      {/* List of admin and other content */}
+      <div className='mt-4 px-5 pt-3'>
+        <h3>List of Admins</h3>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Admin</td>
+              <td>Admin</td>
+            </tr>
+            {/* Add more rows as needed */}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
